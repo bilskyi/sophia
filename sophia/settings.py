04b032a2470f6ca2f479def158d11a399d036036
bilskyi <bilskyioleksandr@outlook.com>
2023-10-16 21:39:36 +0300

    preparing the project

--- sophia/settings.py
+++ sophia/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'apps.core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
