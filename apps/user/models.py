6353adfa1dbb3177abac1f45f0e1c39925fe4361
bilskyi <bilskyioleksandr@outlook.com>
2023-10-17 23:37:17 +0300

    redefinition of base user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,9 +1,52 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 
-class UserProfile(AbstractUser):
-    email = models.EmailField(max_length=255, unique=True)
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email or not password:
+            raise ValueError('Email and password must be provided')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        """ Return string representation of our user """
+        return self.email
