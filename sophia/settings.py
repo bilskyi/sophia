8b3b1559755029e9c3d74eec6ff12aaee4bab889
bilskyi <bilskyioleksandr@outlook.com>
2023-10-20 20:34:58 +0300

    adding swagger docs

--- sophia/settings.py
+++ sophia/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'drf_yasg',
     'apps.core.apps.CoreConfig',
     'apps.user.apps.UserConfig',
 ]
