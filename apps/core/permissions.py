f5e26c62e58adc8ec53b718dacb9ffa1435ff0aa
bilskyi <bilskyioleksandr@outlook.com>
2023-10-20 18:25:28 +0300

    fix the permission in CourseViewSet

--- apps/core/permissions.py
+++ apps/core/permissions.py
@@ -6,14 +6,20 @@ class IsTeacherOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user.role == User.Role.TEACHER
+
+        if request.user.is_authenticated:
+            return request.user.role == User.Role.TEACHER
+        
+        return False
+
     
 
 class IsAdminOrReadOnly(permissions.IsAdminUser):
 
     def has_permission(self, request, view):
         is_admin = super(
-            IsAdminOrReadOnly, 
+            IsAdminOrReadOnly,
             self).has_permission(request, view)
         is_admin = super().has_permission(request, view)
+
         return request.method in permissions.SAFE_METHODS or is_admin
