6801bd9a30ab5812de2ee17a4e147512e51b3053
bilskyi <bilskyioleksandr@outlook.com>
2023-10-22 23:07:29 +0300

    email verification template

--- apps/user/emails.py
+++ apps/user/emails.py
@@ -1,15 +1,15 @@
+from django.template.loader import render_to_string
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import User
 import random
 
-
 def send_otp_via_email(email):
     subject = "Your account verification email"
-    otp = str(random.randint(100, 999)) + " " + str(random.randint(100, 999))
-    message = f'Your otp is {otp}'
+    otp = random.randint(100_000, 999_999)
+    html_message = render_to_string('user/email_verification.html', {'otp': otp})
     email_from = settings.EMAIL_HOST
-    send_mail(subject, message, email_from, [email])
+    send_mail(subject=subject, message='Sophia: You Verification Code Is Here:', html_message=html_message, from_email=email_from, recipient_list=[email])
     user_obj = User.objects.get(email=email)
     user_obj.otp = otp
     user_obj.save()
