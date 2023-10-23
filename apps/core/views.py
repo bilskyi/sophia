0e9a77c4f375650dbe0bf299c9e509d897c96a7a
bilskyi <bilskyioleksandr@outlook.com>
2023-10-23 23:23:56 +0300

    join user to group view

--- apps/core/views.py
+++ apps/core/views.py
@@ -5,6 +5,7 @@ from .permissions import *
 from .models import *
 from rest_framework import generics, permissions, viewsets
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -34,6 +35,25 @@ class GetUsersByGroupViewSet(viewsets.ReadOnlyModelViewSet):
         return User.objects.filter(group_id=group_id)
     
 
-class JoinUserToGroup(APIView):
+class JoinUserToGroupView(APIView):
     def post(self, request):
-        user = request.user
+        serializer = serializers.JoinUserToGroupSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        link_id = serializer.data['link_id']
+ 
+        if Group.objects.filter(link_id=link_id).exists():
+            group = Group.objects.get(link_id=link_id)
+            request.user.group = group
+            request.user.save()
+            
+            return Response({
+                'status': 200,
+                'message': f'User was joined to the group {group.name}',
+                'data': serializer.data
+            })
+        
+        return Response({
+            'status': 404,
+            'message': "We couldn't find such course",
+            "data": serializer.data
+        })
